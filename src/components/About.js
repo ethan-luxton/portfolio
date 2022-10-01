ee13d2093552799996343a3b69d3fd8c06bc0468
ethan-luxton <luxtonethan@hotmail.com>
2022-10-01 16:24:44 -0700

    Fix for warning which stopped netlify from deploying

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,11 @@
 import React, { Component } from "react";
 import { Icon } from "@iconify/react";
-import angularIcon from "@iconify/icons-logos/angular-icon";
+
 import reactIcon from "@iconify/icons-logos/react";
-import vueIcon from "@iconify/icons-logos/vue";
+
 
 class About extends Component {
   render() {
-    if (this.props.sharedBasicInfo) {
-      var profilepic = "images/" + this.props.sharedBasicInfo.image;
-    }
     if (this.props.resumeBasicInfo) {
       var sectionName = this.props.resumeBasicInfo.section_name.about;
       var hello = this.props.resumeBasicInfo.description_header;
